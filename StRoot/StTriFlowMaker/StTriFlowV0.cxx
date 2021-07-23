3154e1daa911374d396aa656b184e769cbc692dc
cdxing <danielxchenx@gmail.com>
2021-07-23 13:12:08 -0400

    check v2

--- StRoot/StTriFlowMaker/StTriFlowV0.cxx
+++ StRoot/StTriFlowMaker/StTriFlowV0.cxx
@@ -319,7 +319,11 @@ void StTriFlowV0::doPhi(Int_t Flag_ME, Int_t cent9, Int_t Bin_vz, Int_t Bin_Psi2
                                TriFlow::pt_low_phi[pt_bin] <= pt && pt <= TriFlow::pt_up_phi[pt_bin])
                                {
                                  mHist_SE_InvM_ptSetA_centSetA[pt_bin][cent]->Fill(InvMassAB);
-                                 if(phi_Psi2 != -999.9 && Res_EP != -999.9)mProfile_v2_reso_ptSetA_centSetA[pt_bin][cent]->Fill(InvMassAB,flow2);
+                                 if(phi_Psi2 != -999.9 && Res_EP != -999.9)
+                                 {
+                                   if(pt_bin==0)std::cout << "ptbin1 flow2 = " << flow2 << std::endl;
+                                   mProfile_v2_reso_ptSetA_centSetA[pt_bin][cent]->Fill(InvMassAB,flow2);
+                                 }
                                }
                             if(TriFlow::cent_low[cent]<= cent9 && cent9 <= TriFlow::cent_up[cent] &&
                                TriFlow::pt_low_phi[pt_bin] <= pt_rot && pt_rot <= TriFlow::pt_up_phi[pt_bin])
