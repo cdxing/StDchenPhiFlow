49aaffda54a592feaa3989e815e660adba8555d9
cdxing <danielxchenx@gmail.com>
2021-07-23 13:30:09 -0400

    double counting?

--- StRoot/StTriFlowMaker/StTriFlowV0.cxx
+++ StRoot/StTriFlowMaker/StTriFlowV0.cxx
@@ -319,9 +319,10 @@ void StTriFlowV0::doPhi(Int_t Flag_ME, Int_t cent9, Int_t Bin_vz, Int_t Bin_Psi2
                                TriFlow::pt_low_phi[pt_bin] <= pt && pt < TriFlow::pt_up_phi[pt_bin])
                                {
                                  mHist_SE_InvM_ptSetA_centSetA[pt_bin][cent]->Fill(InvMassAB);
+                                 std::cout << "invM = " << InvMassAB << std::endl;
                                  if(phi_Psi2 != -999.9 && Res_EP != -999.9 && flow2 != -999.9)
                                  {
-                                   if(pt_bin==0)std::cout << "ptbin1 flow2 = " << flow2 << std::endl;
+                                   // if(pt_bin==0)std::cout << "ptbin1 flow2 = " << flow2 << std::endl;
                                    mProfile_v2_reso_ptSetA_centSetA[pt_bin][cent]->Fill(InvMassAB,flow2);
                                  }
                                }
